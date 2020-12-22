6fa8c9a604d3aa019c477eb0f695e2db6f97fefc
Squalli <virgile.gibello@gmail.com>
2020-12-22 12:00:36 +0100

    Ajout du DAO, du templating et webdesign sur voir.php

--- src/manager/ProductManager.php
+++ src/manager/ProductManager.php
@@ -1,28 +1,14 @@
 <?php
     namespace App\Manager;
 
+    use App\Core\DAO;
     use App\Entity\Product;
 
     /**
      * Gestionnaire de produits en base de données
      */
-    class ProductManager
+    class ProductManager extends DAO
     {
-        private $pdo;
-
-        public function __construct(){
-            $this->pdo = new \PDO(
-                "mysql:host=localhost:3306;dbname=appli",
-                "root",
-                "",
-                [
-                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, //les erreurs venant de MySQL seront des Exception
-                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC //on récupère les données de MySQL dans un tableau associatif
-                    //ex : ['name' => 'Biscuit', 'price' => 25.5]
-                ]
-            );
-        }
-
         /**
          * Récupère tous les produits de la base de données
          * 
